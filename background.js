5a385e1e57f9668185afe0e4b39d3a4a93c84151
Serenasing <serenasing@uvic.ca>
2024-08-25 22:23:51 -0700

    send notifications correctly now. also added icon picture and changed up the html so can start 20min or 20sec timer independently

--- background.js
+++ background.js
@@ -1,12 +1,117 @@
-importScripts('index.js');
-
-chrome.alarms.create("myTimer", {
-    delayInMinutes: 20
-});
-
-chrome.alarms.onAlarm.addListener(function(alarm) {
-    if (alarm.name === 'myTimer') {
-        alert("Time to take an eye break!"); // Alert after 20 minutes
-        console.log("alarm running at " + new Date().getSeconds());
+/*
+chrome.alarms.onAlarm.addListener(
+    () => {
+        if (temp === "20min timer!"){
+            chrome.notifications.create(
+                {
+                    type: 'basic',
+                    iconUrl: 'eye2.png',
+                    title: 'Eye-Break Reminder',
+                    message: "It's been 20min! Time to start 20sec eye break!"
+                },
+                () => { }
+                
+            )
+        } else if (temp === "20sec timer!") {
+            chrome.notifications.create(
+                {
+                    type: 'basic',
+                    iconUrl: 'eye.png',
+                    title: 'Eye-Break Over!',
+                    message: "It's been 20sec! Time to restart the 20min screen time!"
+                },
+                () => { }
+                
+            )
+        }
+        
     }
+)
+   
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.time === 1){
+            create20min();
+        }else {
+            create20sec();
+        }
+    
+    sendResponse({status: "timer set"});
 });
+
+var temp = "timer";
+
+function create20min() {
+    chrome.alarms.create(
+        "20min timer!",
+        {
+          delayInMinutes: 1/3
+        }
+        
+    );
+    temp = "20min timer!";
+}
+
+function create20sec() {
+    chrome.alarms.create(
+        "20sec timer!",
+        {
+          delayInMinutes: 1/3
+        }
+        
+    );
+    temp = "20sec timer!";
+}
+*/
+
+chrome.alarms.onAlarm.addListener(
+    (alarm) => {  // Use 'alarm' parameter here
+        console.log("Alarm triggered:", alarm.name); 
+        if (alarm.name === "20min timer!") {
+            chrome.notifications.create(
+                {
+                    type: 'basic',
+                    iconUrl: 'eye2.png',
+                    title: 'Eye-Break Reminder',
+                    message: "It's been 20min! Time to start your 20sec eye break!"
+                },
+                () => { }
+            );
+        } else if (alarm.name === "20sec timer!") {
+            chrome.notifications.create(
+                {
+                    type: 'basic',
+                    iconUrl: 'eye.png',
+                    title: 'Eye-Break Over!',
+                    message: "It's been 20sec! Time to restart your 20min screen time!"
+                },
+                () => { }
+            );
+        }
+    }
+);
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.time === "1") {
+            create20min();
+        } else if (request.time === "2") {
+            create20sec();
+        }
+
+        sendResponse({ status: "timer set" });
+    }
+);
+
+function create20min() {
+    chrome.alarms.create("20min timer!", {
+        delayInMinutes: 20 // Corrected to 20 minutes for the 20min timer
+    });
+}
+
+function create20sec() {
+    chrome.alarms.create("20sec timer!", {
+        delayInMinutes: 1/3  // 20 seconds
+    });
+}
--- index.js
+++ index.js
@@ -1,73 +1,61 @@
 var timerElement = document.getElementById('timer');
 var timer;
 const breakElement = document.querySelector('.twenty-second-break');
-const twentyElement = document.querySelector('.twenty-minutes');
+const containerElement = document.getElementById('timer-container');
 document.getElementById("startButten").addEventListener("click", start);
-document.getElementById("pauseButten").addEventListener("click", pause);
+document.getElementById("start20sButten").addEventListener("click", start20s);
 
 function start(){
-    var sec = 0;
-    var min = 0;
-    clearInterval(timer); // Clear any existing timer
-    timer = setInterval(() => {
-        // Format minutes and seconds with leading zeros if needed
-        const formattedSec = sec < 10 ? `0${sec}` : sec;
-        const formattedMin = min < 10 ? `0${min}` : min;
-        
-        timerElement.innerHTML = `${formattedMin}:${formattedSec}`
-        sec++;
-        if (sec === 60){
-            sec = 0;
-            min++;
-        }
-        if(min === 20){
-            alert("Time to take an eye break!");
-            clearInterval(timer); // Stop the timer
-            breakTimer(); // Show break element
-            return; // Exit the setInterval callback
-        }
-    }, 10)
-}
-
-function pause(){
-    clearInterval(timer);
-}
-
-function breakTimer() {
-    twentyElement.innerHTML = `
-        <h3>Ready for your 20s break?</h3>
-        <button id="start20sButton">Start</button>
+    containerElement.innerHTML = `
+        <h3 class="text">Timer started...</h3>
+        <button id="stopButten" >stop</button>  
     `;
+    document.getElementById("stopButten").addEventListener("click", stop);
+    chrome.runtime.sendMessage({ time: "1"}, function (response){
+        console.log(response);
+    })
+    clearInterval(timer); // Stop the timer
+    return; // Exit the setInterval callback
+    
+    
+}
 
-    document.getElementById("start20sButton").addEventListener("click", start20s);
-    timerElement.innerHTML = '00:00';
+function stop(){
+    clearInterval(timer);
+    sec = 0;
+    min = 0;
+    containerElement.innerHTML = `
+        <div class="play-pause">
+           <h3 class="text">START:</h3>
+        </div>
+        <div class="twenty-minutes">
+            <button id="startButten" >20 min screen time</button>
+            <button id="start20sButten" >20 sec eye-break</button> 
+        </div>
+    `;
+    document.getElementById("startButten").addEventListener("click", start);
+    document.getElementById("start20sButten").addEventListener("click", start20s);
 }
 
 function startTimer() {
     twentyElement.innerHTML = `
-        <button id="startButten">Start</button>
-        <button id="pauseButten">Pause</button>
+        <h3 class="text">Start 20min screen time?</h3>
+        <button id="startButten" >start</button>  
     `;
-    timerElement.innerHTML = '00:00';
     
     document.getElementById("startButten").addEventListener("click", start);
-    document.getElementById("pauseButten").addEventListener("click", pause);
+
 }
 
 function start20s(){
-    startTimer();
-    var sec = 0;
-    clearInterval(timer); // Clear any existing timer
-    timer = setInterval(() => {
-        // Format minutes and seconds with leading zeros if needed
-        const formattedSec = sec < 10 ? `0${sec}` : sec;
-        
-        timerElement.innerHTML = `00:${formattedSec}`
-        sec++;
-        if (sec === 20){
-            alert("Break time over!");
-            clearInterval(timer); // Stop the timer
-            return; // Exit the setInterval callback
-        }
-    }, 1000)
+    containerElement.innerHTML = `
+        <h3 class="text">Timer started...</h3>
+        <button id="stopButten" >stop</button>  
+    `;
+    
+        chrome.runtime.sendMessage({ time: "2"}, function (response){
+            console.log(response);
+        })
+        clearInterval(timer); // Stop the timer
+        return;
 }
